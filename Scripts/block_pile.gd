73fc54328b22e6fecc0452807f80ef94ef26ac29
Thiago Borges <thiago@tfcb93.com>
2024-10-08 17:10:47 -0300

    Adds font; Removes more old code from files

--- Scripts/block_pile.gd
+++ Scripts/block_pile.gd
@@ -1,17 +1,7 @@
 extends Node2D;
 
 @onready var start_pos := $"Start Pile Position";
-@onready var tap_sound := $"Tap Sound";
-@onready var block_sounds := $"Block Sounds";
-@onready var destroy_block_sound := $"Destroy Block Sound";
-
 @onready var life_value := $interface/life_value;
-
-var pile_types: Array[String] = [];
-var taps_since_new_block := 0;
-var total_blocks_generated := 0;
-
-# ====
 @onready var top_block := $top_block;
 @onready var under_block := $under_block;
 
@@ -29,46 +19,6 @@ func _ready() -> void:
 
 	Events.connect("pause_game", _on_pause_game);
 	Events.connect("unpause_game", _on_unpause_game);
-	# Events.connect("tap",_on_screen_tap);
-	# life_value.text = str(blocks[0].life);
-
-	# for i in range(len(pile_types)):
-	# 	var newBlock = block_scene.instantiate();
-	# 	newBlock.position = start_pos.position + Vector2(0.0, block_gap * i);
-	# 	newBlock.block_type = pile_types[i];
-	# 	pile.add_child(newBlock);
-	# 	pile.move_child(newBlock, 0);
-		
-	# #load_block_sounds();
-	# set_actual_block(pile.get_child(-1).block_type);
-
-# func _on_screen_tap() -> void:
-# 	if not Globals.game_stop:
-# 		#taps_since_new_block += 1;
-# 		#if taps_since_new_block >= pile.get_child(-1).life:
-# 		if pile.get_child(-1).life <= 0:
-# 			#play_block_sound(pile.get_child(-1).block_type);
-# 			destroy_block_sound.pitch_scale = randf_range(1.0, 0.7);
-# 			destroy_block_sound.play();
-# 			pile.get_child(-1).queue_free();
-# 			taps_since_new_block = 0;
-# 			Globals.total_blocks_destroyed += 1;
-# 			pile_types.pop_front();
-# 			set_actual_block(pile_types[0]);
-			
-# 			Events.emit_signal("add_bits");
-# 			Globals.actual_block_points = 0;
-			
-# 			if pile.get_child_count() - 1 == 0:
-# 				Globals.game_stop = true;
-			
-# 			generate_block_pile();
-# 			insert_new_block();
-			
-# 			lift_pile_up();
-# 		else:
-# 			tap_sound.pitch_scale = randf_range(3.0, 1.0);
-# 			tap_sound.play();
 
 func _on_generate_pile() -> void:
 	if (not sequence):
@@ -114,20 +64,6 @@ func _on_hit_block(tool_resistance: int) -> void:
 func calculate_player_points() -> void:
 	var total_points_earned = 1;
 	Events.emit_signal("earn_points", total_points_earned);
-
-#func load_block_sounds() -> void:
-	#for block_type in Globals.block_types:
-		#var newSound = AudioStreamPlayer.new();
-		#newSound.stream  = load("res://Resources/Sounds/" + block_type + ".wav");
-		#newSound.name = block_type;
-		#block_sounds.add_child(newSound);
-		#
-#
-#func play_block_sound(type: String) -> void:
-	#get_node("Block Sounds/" + type).play();
-
-# func set_actual_block(type: String) -> void:
-# 	Globals.actual_block_type = type;
 
 func _on_pause_game() -> void:
 	%interface.visible = false;
--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,47 +1,17 @@
 extends Node2D
 
 @onready var player := $Player;
-@onready var block_pile := $"Block Pile";
 
 var counter_value := 0;
 
 func _ready() -> void:
 	load_blocks_into_memory();
-	# counters_node.visible = false;
-	# game_over_screen.visible = false;
-	# selector.visible = false;
-	# shop_button.visible = false;
-	# var screensize := get_viewport_rect().size;
-	
-	# Events.connect("discount_bits", _on_bought_item);
-	# Events.connect("add_bits", _on_add_bits);
 
 func _unhandled_input(event: InputEvent) -> void:
 	if (event.is_action_pressed("action")):
 		match Globals.game_state:
 			Globals.GAME_STATES.IN_GAME:
 				Events.emit_signal("hit_block", player.player_tool.resistance);
-	# if not Globals.game_stop:
-	# 	if event is InputEventScreenTouch and event.is_pressed():
-	# 		if not Globals.game_start:
-	# 			Globals.game_start = true;
-	# 			$"Start Screen".visible = false;
-	# 			counters_node.visible = true;
-	# 			selector.visible = true;
-	# 			shop_button.visible = true;
-	# 		else:
-	# 			Globals.total_taps += 1;
-	# 			block_tap_action();
-	# 			Events.emit_signal("tap");
-
-# Do the calculations with the actual block and the actual pickaxe
-# func block_tap_action() -> void:
-# 	var data = BAP.blocks_info[Globals.actual_block_type].point_status[pickaxe_bag.actual_pickaxe_name];
-# 	var pickaxe_damage = BAP.pickaxe_information[pickaxe_bag.actual_pickaxe_name].life_at_start if typeof(data[1]) == TYPE_STRING and data[1] == "MAX" else data[1];
-	
-# 	block_pile.set_actual_block_damage(data[0]);
-# 	pickaxe_bag.set_actual_pickaxe_damage(pickaxe_damage);
-# 	Globals.actual_block_points += data[2];
 
 func load_blocks_into_memory() -> void:
 	var block_files := DirAccess.open("res://Resources/blocks").get_files();
