373c05c6f313a128c52f666fb6969398a14c38d8
Thiago Borges <thiago@tfcb93.com>
2024-10-08 14:22:13 -0300

    Removes from project old resources and code that might not be used

--- Scripts/central_screen_text.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer;
-
-@onready var center_label := $"Center Label";
-	
-func _on_win() -> void:
-	center_label.text = "YOU WIN";
-	
-func _on_lose() -> void:
-	center_label.text = "YOU LOSE";
